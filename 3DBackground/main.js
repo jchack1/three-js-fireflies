ee37fc1be3ebc52cfb172f71b7814d61909ba8d7
jchack1 <55161597+jchack1@users.noreply.github.com>
2023-02-20 22:21:08 -0700

    initial upload

---
+++ 3DBackground/main.js
@@ -0,0 +1,89 @@
+//lots of the code based on bubbles in this example: https://threejs.org/examples/#webgl_effects_anaglyph
+//some help with lighting etc from this tutorial https://www.youtube.com/watch?v=Q7AOvWpIVHU
+
+import * as THREE from "three";
+import "/styles.css";
+let camera, scene, renderer;
+
+const spheres = [];
+
+init();
+animate();
+
+function init() {
+  //set up camera
+  //args: (1)field of view, (2)aspect ratio, (3 + 4)view frustrum(what's visible relative to the camera )
+  camera = new THREE.PerspectiveCamera(
+    55,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.z = 3;
+  camera.focalLength = 30;
+
+  //set up scene and background
+
+  scene = new THREE.Scene();
+
+  const forestTexture = new THREE.TextureLoader().load("forestDusk.jpg");
+
+  scene.background = forestTexture;
+
+  //begin making spheres
+
+  const geometry = new THREE.SphereGeometry(0.015, 30, 6);
+  const material = new THREE.MeshStandardMaterial({color: 0xb3f266});
+
+  //setting position of each sphere
+  for (let i = 0; i < 700; i++) {
+    const mesh = new THREE.Mesh(geometry, material);
+
+    mesh.position.x = Math.random() * 10 - 5;
+    mesh.position.y = Math.random();
+    mesh.position.z = Math.random() * 10 - 5;
+
+    mesh.scale.x = mesh.scale.y = mesh.scale.z = Math.random() * 2 + 1;
+
+    scene.add(mesh);
+
+    spheres.push(mesh);
+  }
+
+  //adding light
+  //point light may not be needed, but gives spheres 3D effect, not just solid circles floating around
+  const pointLight = new THREE.PointLight(0xf1fab6);
+  pointLight.position.set(5, 5, 5);
+
+  const ambientLight = new THREE.AmbientLight(0xf1fab6);
+  scene.add(pointLight, ambientLight);
+
+  //set up renderer
+  //make it full screen, set pixel ratio to be same as device
+  renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector("#bg"),
+  });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  render();
+}
+
+function render() {
+  //the part I really needed help with, making spheres move around the screen randomly
+
+  const timer = 0.0001 * Date.now();
+
+  for (let i = 0, il = spheres.length; i < il; i++) {
+    const sphere = spheres[i];
+
+    sphere.position.x = 6 * Math.cos(timer + i);
+    sphere.position.y = 2 * Math.sin(timer + i * 0.9);
+  }
+
+  renderer.render(scene, camera);
+}
